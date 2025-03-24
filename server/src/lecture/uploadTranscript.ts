59f8187cf44621529de7ab6af320eb32579bf7a3
dzl-i <denzel.iskandar@gmail.com>
2025-03-25 02:19:10 +1100

    feat: create lecture by uploading transcript

---
+++ server/src/lecture/uploadTranscript.ts
@@ -0,0 +1,31 @@
+import { createFlashcard, createLecture, createSection, getLectureDetail } from "../helper/lectureHelper";
+
+export async function lectureUploadTranscript(title: string, transcript: string) {
+  const result: any = await fetch(`chatgpt`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ transcript: transcript })
+  }); // TODO: Replace with ChatGPT API endpoint
+
+  if (result.status !== 200) throw { status: 500, message: "An error occurred." };
+  const response = await result.json();
+
+  const lecture = await createLecture(title);
+
+  for (const section of response.sections) {
+    const newSection = await createSection(lecture.id, section.title, section.transcript, section.summary, section.graph);
+    if (newSection === null) throw { status: 500, message: "An error occurred." };
+
+    for (const flashcard of section.flashcards) {
+      const newFlashcard = await createFlashcard(newSection.id, flashcard.question, flashcard.answer);
+      if (newFlashcard === null) throw { status: 500, message: "An error occurred." };
+    }
+  }
+
+  const lectureDetails = await getLectureDetail(lecture.id);
+  if (lectureDetails === null) throw { status: 500, message: "An error occurred." };
+
+  return lectureDetails;
+}
