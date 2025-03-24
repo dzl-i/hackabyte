39eea92633c242321afc12a2642311b651eaf972
Madhu Shrestha <madhu.shrestha666@gmail.com>
2025-03-24 19:39:27 +1100

    frontend setup

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,7 +4,11 @@ import { Button } from "../components/ui/button";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Button>hi</Button>
+      <img src="./logo.svg"></img>
+
+      <p className="text-xs italic text-white">
+        Please upload your content to get started.
+      </p>
     </div>
   );
 }
