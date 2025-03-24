e4800766beb6f2a6cf7813394be0ab283c95ed1e
Merry Rosalie <merryrosalie001@gmail.com>
2025-03-25 00:19:29 +1100

    rebased and restored backend files

--- frontend/src/app/lectures/page.tsx
+++ frontend/src/app/lectures/page.tsx
@@ -1,45 +1,26 @@
-"use client";
-
-import React, { useMemo, useState } from "react";
+import React from "react";
 import { Search } from "lucide-react";
 import LectureItem from "@/components/LectureItem";
 import NavBar from "@/components/NavBar";
 
-const lectures = [
-  { id: "1", name: "Rule Utalitarian", lastUpdated: "Uploaded 1 hour ago" },
-  { id: "2", name: "Rule Utalitarian", lastUpdated: "Uploaded 1 hour ago" },
-  { id: "3", name: "yippee", lastUpdated: "Uploaded 1 hour ago" },
-];
-
-export default function Page() {
-  const [searchText, setSearchText] = useState("");
-
-  const memoizedLectures = useMemo(
-    () =>
-      lectures.filter((lecture) =>
-        lecture.name.toLowerCase().includes(searchText.toLowerCase())
-      ),
-    [lectures, searchText]
-  );
+function page() {
+  const lectures = [
+    { title: "Rule Utalitarian", lastUpdated: "Uploaded 1 hour ago" },
+    { title: "Rule Utalitarian", lastUpdated: "Uploaded 1 hour ago" },
+    { title: "Rule Utalitarian", lastUpdated: "Uploaded 1 hour ago" },
+  ];
 
   return (
     <div>
       <NavBar />
 
-      <div className="flex flex-col items-center w-full max-w-4xl mx-auto min-h-screen overflow-hidden px-6 py-8 gap-6">
+      <div className="flex flex-col justify-center w-full max-w-4xl mx-auto min-h-screen overflow-hidden p-6 py-16 gap-16">
         <div className="flex flex-row bg-[#1C1C24] w-full h-12 items-center pl-4 rounded-md gap-x-2 text-[#FFFEFD]/50">
-          <Search size={16} className="text-white" />
-          <input
-            className="w-full outline-none"
-            type="text"
-            placeholder="Search..."
-            value={searchText}
-            onChange={(e) => setSearchText(e.target.value)}
-          />
+          <Search size={16} className="text-white" /> Search...
         </div>
 
         <div className="space-y-2 w-full">
-          {memoizedLectures.map((lecture, index) => (
+          {lectures.map((lecture, index) => (
             <LectureItem key={index} lecture={lecture} />
           ))}
         </div>
@@ -47,3 +28,5 @@ export default function Page() {
     </div>
   );
 }
+
+export default page;
