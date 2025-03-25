94bf8a65c13aa7bdbb607f91d6dcca4744b7739b
Deki <z5268870@student.unsw.edu.au>
2025-03-25 15:29:43 +1100

    Added switching topic functionalaity

--- frontend/src/app/lectures/[id]/page.tsx
+++ frontend/src/app/lectures/[id]/page.tsx
@@ -6,98 +6,119 @@ import { useRouter } from "next/navigation"
 import React, { useState } from "react"
 import FlashCard from "@/components/FlashCard"
 
-export type LectureTabs = {
-	name: string
-	icon: React.ReactNode
-	content: React.ReactNode
-}
-
-// DATABASE
-const flashcards = [
-	{ question: "how is merry's mental state?", answer: "not good" },
-	{ question: "how is merry's mental state?", answer: "not good" },
-	{ question: "how is merry's mental state?", answer: "not good" },
-]
-
-const summary =
-	"Utilitarianism is a consequentialist ethical theory that holds the view that the morality of an action is determined by its outcomes—specifically, the extent to which it promotes overall happiness or minimizes suffering. Within this broader framework, Act Utilitarianism evaluates the morality of individual actions based on the direct consequences they produce. In contrast to Rule Utilitarianism, which assesses actions based on whether they align with rules that generally promote utility, Act Utilitarianism requires one to consider each situation uniquely."
-
-const transcripts = [
-	{
-		timestamp: "00:01",
-		text: "Okay, so… Utilitarianism is what we call a consequentialist ethical theory.",
-	},
-	{
-		timestamp: "00:01",
-		text: "Okay, so… Utilitarianism is what we call a consequentialist ethical theory.",
-	},
-	{
-		timestamp: "200:01",
-		text: "Okay, so… Utilitarianism is what we call a consequentialist ethical theory.",
-	},
-]
-
-const lectureTabs = [
-	{
-		name: "Summary",
-		icon: Heart,
-		content: (
-			<div className="px-4">
-				<h4 className="text-lg font-bold text-white mb-4">Summary</h4>
-				<p className="text-white">{summary}</p>
-			</div>
-		),
-	},
-	{
-		name: "Transcript",
-		icon: Captions,
-		content: (
-			<div className="px-4">
-				<h4 className="text-lg font-bold text-white mb-4">Transcript</h4>
-				<div className="grid grid-cols-[80px_1fr] gap-y-2">
-					{transcripts.map((transcript, index) => (
-						<React.Fragment key={index}>
-							<p className="text-gray-400 font-mono">
-								[{transcript.timestamp}]
-							</p>
-							<p className="text-white">{transcript.text}</p>
-						</React.Fragment>
-					))}
-				</div>
-			</div>
-		),
-	},
-	{
-		name: "Flashcards",
-		icon: Layers,
-		content: (
-			<div className="px-4">
-				<h4 className="text-lg font-bold text-white mb-4">Flashcards</h4>
-
-				<div className="space-y-4">
-					{flashcards.map((flashcard, index) => (
-						<FlashCard
-							key={index}
-							question={flashcard.question}
-							answer={flashcard.answer}
-						/>
-					))}
-				</div>
-			</div>
-		),
-	},
-]
-
 export default function Page() {
 	const router = useRouter()
 
-	const [selectedTab, setSelectedTab] = useState(lectureTabs[0])
+	// DATABASE
+	const flashcardsDummy = [
+		{ question: "how is merry's mental state?", answer: "not good" },
+		{ question: "how is merry's mental state?", answer: "not good" },
+		{ question: "how is merry's mental state?", answer: "not good" },
+	]
+
+	const summaryDummy =
+		"Utilitarianism is a consequentialist ethical theory that holds the view that the morality of an action is determined by its outcomes—specifically, the extent to which it promotes overall happiness or minimizes suffering..."
+
+	const transcriptsDummy = [
+		{
+			timestamp: "00:01",
+			text: "Okay, so… Utilitarianism is what we call a consequentialist ethical theory.",
+		},
+		{
+			timestamp: "00:01",
+			text: "Okay, so… Utilitarianism is what we call a consequentialist ethical theory.",
+		},
+		{
+			timestamp: "200:01",
+			text: "Okay, so… Utilitarianism is what we call a consequentialist ethical theory.",
+		},
+	]
+
+	const transcriptsDummy2 = [
+		{
+			timestamp: "00:01",
+			text: "Test",
+		},
+	]
+
+	const sections = [
+		{
+			name: "Rule & Act Utilitarianism",
+			summary: summaryDummy,
+			transcripts: transcriptsDummy,
+			flashcards: flashcardsDummy,
+		},
+		{
+			name: "Rule & Act Utilitarianism 2",
+			summary: "different one trust",
+			transcripts: transcriptsDummy2,
+			flashcards: flashcardsDummy,
+		},
+	]
+
+	// Section and tab state
+	const [topicTab, setTopicTab] = useState(sections[0])
+	const [selectedTabIndex, setSelectedTabIndex] = useState(0)
+
+	// Dynamically generates lecture type tab
+	const lectureTabs = [
+		{
+			name: "Summary",
+			icon: Heart,
+			content: (
+				<div className="px-4">
+					<h4 className="text-lg font-bold text-white mb-4">Summary</h4>
+					<p className="text-white">{topicTab.summary}</p>
+				</div>
+			),
+		},
+		{
+			name: "Transcript",
+			icon: Captions,
+			content: (
+				<div className="px-4">
+					<h4 className="text-lg font-bold text-white mb-4">Transcript</h4>
+					<div className="grid grid-cols-[80px_1fr] gap-y-2">
+						{topicTab.transcripts.map((transcript, index) => (
+							<React.Fragment key={index}>
+								<p className="text-gray-400 font-mono">
+									[{transcript.timestamp}]
+								</p>
+								<p className="text-white">{transcript.text}</p>
+							</React.Fragment>
+						))}
+					</div>
+				</div>
+			),
+		},
+		{
+			name: "Flashcards",
+			icon: Layers,
+			content: (
+				<div className="px-4">
+					<h4 className="text-lg font-bold text-white mb-4">Flashcards</h4>
+
+					<div className="space-y-4">
+						{topicTab.flashcards.map((flashcard, index) => (
+							<FlashCard
+								key={index}
+								question={flashcard.question}
+								answer={flashcard.answer}
+							/>
+						))}
+					</div>
+				</div>
+			),
+		},
+	]
+
+	const selectedTab = lectureTabs[selectedTabIndex]
 
 	return (
 		<main>
 			<NavBar />
 			<main className="p-6 flex gap-8">
-				{/* List of sections */}
+				{/* Sidebar: Topics */}
 				<section className="space-y-6 flex-1 min-w-[220px]">
 					<button
 						onClick={() => router.back()}
@@ -105,31 +126,36 @@ export default function Page() {
 					>
 						<ArrowLeft className="w-4 h-4" /> Back
 					</button>
-
 					<div className="max-w-xs space-y-2">
 						<p className="text-white/75 font-bold uppercase">Sections</p>
 						<div className="flex flex-col gap-1">
-							<button className="text-white cursor-pointer text-left text-ellipsis font-bold py-2 px-4 duration-200 bg-[rgba(32,_33,_42,_1)] rounded-lg">
-								Rule & Act Utilitarianism
-							</button>
-							<button className="text-white cursor-pointer text-left text-ellipsis py-2 px-4 duration-200 hover:bg-white/5 rounded-lg">
-								Rule & Act Utilitarianism
-							</button>
+							{sections.map((section, index) => (
+								<button
+									key={index}
+									onClick={() => setTopicTab(section)}
+									className={`flex gap-2 px-4 py-2 rounded-lg duration-200 ${
+										topicTab.name === section.name
+											? "bg-[rgba(32,_33,_42,_1)] text-white font-bold"
+											: "text-gray-400 hover:text-white hover:bg-[rgba(32,_33,_42,_1)]"
+									}`}
+								>
+									<p>{section.name}</p>
+								</button>
+							))}
 						</div>
 					</div>
 				</section>
+
+				{/* Main Panel */}
 				<section className="space-y-4 w-full">
-					<h1 className="text-white font-bold text-2xl">
-						Rule & Act Utilitarianism
-					</h1>
+					<h1 className="text-white font-bold text-2xl">{topicTab.name}</h1>
 					<div className="flex gap-2">
-						{lectureTabs.map((tab) => (
+						{lectureTabs.map((tab, index) => (
 							<button
 								key={tab.name}
-								onClick={() => setSelectedTab(tab)}
-								className={`flex justify-center items-center gap-2 px-4 py-2 relative cursor-pointer overflow-clip rounded-lg duration-200
-                ${
-									selectedTab.name === tab.name
+								onClick={() => setSelectedTabIndex(index)}
+								className={`flex justify-center items-center gap-2 px-4 py-2 rounded-lg duration-200 ${
+									selectedTabIndex === index
 										? "bg-[rgba(32,_33,_42,_1)] text-white"
 										: "text-gray-400 hover:text-white hover:bg-[rgba(32,_33,_42,_1)]"
 								}`}
