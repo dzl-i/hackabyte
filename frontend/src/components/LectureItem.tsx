884f9067f0b16a22f4222e6808d7e2bea66a71bc
Merry Rosalie <merryrosalie001@gmail.com>
2025-03-25 23:27:44 +1100

    fuck it we ball

--- frontend/src/components/LectureItem.tsx
+++ frontend/src/components/LectureItem.tsx
@@ -1,16 +1,24 @@
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export type Lecture = {
+  id: string;
   title: string;
-  lastUpdated: string;
+  createdAt: string;
 };
 
 function LectureItem({ lecture }: { lecture: Lecture }) {
+  const router = useRouter();
   return (
-    <div className="flex flex-col bg-[#262934] hover:bg-[#1c1e27] duration-200 w-full py-3 px-4 gap-y-1 rounded-md">
+    <button
+      onClick={() => router.push(`/lectures/${lecture.id}`)}
+      className="flex flex-col bg-[#262934] hover:bg-[#1c1e27] duration-200 text-left w-full py-3 px-4 gap-y-1 rounded-md"
+    >
       <p className="text-md font-bold text-white">{lecture.title}</p>
-      <p className="text-sm text-zinc-500">{lecture.lastUpdated}</p>
-    </div>
+      <p className="text-sm text-zinc-500">
+        Updated at {new Date(lecture.createdAt).toLocaleString()}
+      </p>
+    </button>
   );
 }
 
