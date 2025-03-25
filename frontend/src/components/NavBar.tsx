a6efb49ce07f67eba72d0a8be034d6b8cf9d2ca0
Madhu Shrestha <madhu.shrestha666@gmail.com>
2025-03-25 14:27:55 +1100

    search bar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,13 +1,17 @@
 import { Captions, Video } from "lucide-react";
 import { Bakbak_One } from "next/font/google";
+import { useRouter } from "next/navigation";
 
 const bakbak = Bakbak_One({ weight: "400", subsets: ["latin"] });
 
 export default function NavBar() {
+  const router = useRouter();
+
   return (
     <nav className="flex justify-between items-center py-4 px-6 border border-b border-white/15">
       <h1
-        className={`text-white font-bold text-2xl uppercase ${bakbak.className}`}
+        className={`text-white font-bold text-2xl cursor-pointer uppercase ${bakbak.className}`}
+        onClick={() => router.push("/")}
       >
         SmartScribe
       </h1>
